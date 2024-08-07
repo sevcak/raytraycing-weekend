56289797d56816926fcba2d6381746de74f8843d
sevcak <sevclukas0@gmail.com>
2024-08-07 17:57:39 +0200

    Progress: Camera set up

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,45 @@
 #include <vector>
 
 #include "stb_include.h"
+
 #include "color.h"
-// #include "vec3.h"
+#include "ray.h"
+#include "vec3.h"
+
+color ray_color( const ray& r )
+{
+    vec3 unit_direction = unit_vector( r.direction() );
+    auto a = 0.5 * ( unit_direction.y() + 1.0 );
+    return ( 1.0 - a ) * color( 1.0, 1.0, 1.0 ) + ( a * color( 0.5, 0.7, 1.0 ) );
+}
 
 int main( void )
 {
-    int image_width  = 256;
-    int image_height = 256;
+    auto aspect_ratio = 16.0 / 9.0;
+    int  image_width  = 400;
+    int  image_height = int( image_width / aspect_ratio );
+    image_height = image_height < 1 ? 1 : image_height;
+
+    auto focal_length    = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width  = viewport_height * ( double( image_width ) / image_height );
+    auto camera_center   = point3( 0, 0, 0 );
+
+    /* Horizontal and vertical viewport edge vectors */
+    auto viewport_u = vec3( viewport_width, 0, 0 );
+    auto viewport_v = vec3( 0, -viewport_height, 0 );
+
+    /* Horizontal and vertical pixel-to=pixel delta vectors */
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center
+                               - vec3( 0, 0, focal_length )
+                               - viewport_u/2
+                               - viewport_v/2;
+
+    auto pixel_0_0_location = viewport_upper_left
+                              + ( 0.5 * ( pixel_delta_u + pixel_delta_v ) );
 
     std::vector<unsigned char> pixels(image_width * image_height * 3);
 
@@ -19,10 +51,15 @@ int main( void )
                   << ( image_height - i ) << ' ' << std::flush;
 
         for ( int j = 0; j < image_width; ++j ) {
+            auto pixel_center = pixel_0_0_location
+                                + ( j * pixel_delta_u )
+                                + ( i * pixel_delta_v );
+            auto ray_direction = pixel_center - camera_center;
+            ray r( camera_center, ray_direction );
+
+            auto   pixel_color = ray_color( r );
             size_t pixel_index = ( i * image_width + j ) * 3;
-            auto   pixel_color = color( double( j ) / ( image_width  - 1 ),
-                                        double( i ) / ( image_height - 1 ),
-                                        0.0 );
+
             write_color( pixels, pixel_index, pixel_color );
         }
     }
