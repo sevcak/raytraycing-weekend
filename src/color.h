4c9096d4d9e3958597be600ef713b70773721dc6
sevcak <sevclukas0@gmail.com>
2024-08-09 16:58:40 +0200

    Progress: Gamma transform

--- src/color.h
+++ src/color.h
@@ -8,12 +8,25 @@
 
 using color = vec3;
 
+inline double linear_to_gamma( double linear_component )
+{
+    if ( linear_component > 0 ) {
+        return std::sqrt( linear_component );
+    }
+
+    return 0;
+}
+
 inline void write_color( std::vector<unsigned char>& pixels,
                          size_t pixel_index, const color& pixel_color )
 {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    r = linear_to_gamma( r );
+    g = linear_to_gamma( g );
+    b = linear_to_gamma( b );
 
     /* Translate [0, 1] to [0, 255] */
     static const interval intensity( 0.000, 0.999 );
