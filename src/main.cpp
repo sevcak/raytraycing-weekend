3b93c6b7ab2b79ca8801cb74dcb179b7000c64c9
sevcak <sevclukas0@gmail.com>
2024-08-12 13:25:18 +0200

    Progress: Snell's law

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,11 @@
 #include "color.h"
 #include "rtweekend.h"
 
-#include "hittable.h"
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
 #include "camera.h"
 #include "vec3.h"
-
-color ray_color( const ray& r, const hittable& world )
-{
-    hit_record rec;
-    if ( world.hit( r, interval( 0, infinity ), rec ) ) {
-        return 0.5 * ( rec.normal + color( 1, 1, 1 ) );
-    }
-
-    vec3 unit_direction = unit_vector( r.direction() );
-    auto a = 0.5 * ( unit_direction.y() + 1.0 );
-    return ( ( 1.0 - a ) * color( 1.0, 1.0, 1.0 ) ) + ( a * color( 0.5, 0.7, 1.0 ) );
-}
 
 int main( void )
 {
@@ -28,8 +15,8 @@ int main( void )
 
     auto mat_ground = make_shared<lambertian>( color( 0.8, 0.8, 0.0 ) );
     auto mat_center = make_shared<lambertian>( color( 0.1, 0.2, 0.5 ) );
-    auto mat_left   = make_shared<metal>(      color( 0.8, 0.8, 0.8 ) );
-    auto mat_right  = make_shared<metal>(      color( 0.8, 0.6, 0.2 ) );
+    auto mat_left   = make_shared<dielectric>( 1.50 );
+    auto mat_right  = make_shared<metal>( color( 0.8, 0.6, 0.2 ), 1.0 );
 
     world.add( make_shared<sphere>( point3(  0.0, -100.6, -1.0 ), 100.0, mat_ground ) );
     world.add( make_shared<sphere>( point3(  0.0,    0.0, -1.2 ),   0.5, mat_center ) );
--- src/material.h
+++ src/material.h
@@ -4,6 +4,7 @@
 #include "hittable.h"
 #include "ray.h"
 #include "color.h"
+#include "vec3.h"
 
 class hit_record;
 
@@ -46,20 +47,47 @@ private:
 class metal : public material
 {
 public:
-    metal( const color& albedo ) : albedo( albedo ) {}
+    metal( const color& albedo, double fuzz )
+        : albedo( albedo ), fuzz( fuzz < 1 ? fuzz : 1 ) {}
 
     bool scatter( const ray& r_in, const hit_record& rec, color& attenuation,
                   ray& scattered ) const override
     {
         vec3 reflected = reflect( r_in.direction(), rec.normal );
+        reflected = unit_vector( reflected ) + ( fuzz * random_unit_vector() );
+
         scattered   = ray( rec.p, reflected );
         attenuation = albedo;
+
+        return ( dot( scattered.direction(), rec.normal ) > 0 );
+    }
+
+private:
+    color  albedo;
+    double fuzz;
+};
+
+class dielectric : public material
+{
+public:
+    dielectric( double refraction_index ) : refraction_index( refraction_index ) {}
+
+    bool scatter( const ray& r_in, const hit_record& rec, color& attenuation,
+                  ray& scattered ) const override
+    {
+        attenuation = color( 1.0, 1.0, 1.0 );
+        double r_i = rec.front_face ? ( 1.0 / refraction_index ) : refraction_index;
+
+        vec3 unit_direction = unit_vector( r_in.direction() );
+        vec3 refracted = refract( unit_direction, rec.normal, r_i );
+
+        scattered = ray( rec.p, refracted );
 
         return true;
     }
 
 private:
-    color albedo;
+    double refraction_index;
 };
 
 #endif
